41ecf9829b5fd60b8f5753763c2f631fbd00cf56
FIRST_NAME LAST_NAME <nishant@navigus.in>
2021-11-10 21:38:35 +0530

    multiple generics data type

---
+++ src/main/com/github/kaushiknishant/MultipleGenerics.java
@@ -0,0 +1,27 @@
+package main.com.github.kaushiknishant;
+
+// Two generics are used here
+class Table<K,V>{
+    private K key;
+    private V value;
+
+    public Table(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Table{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+}
+
+public class MultipleGenerics {
+    public static void main(String[] args) {
+        Table<String,Integer> table = new Table<>("INR",700);
+        System.out.println(table);
+    }
+}
